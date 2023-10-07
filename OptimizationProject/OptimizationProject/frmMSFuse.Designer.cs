9d03ab74dcab37289006964dd6ed2019ff031d9f
Kabindra <shresthakk2020@gmail.com>
2023-10-07 12:50:10 -0500

    Sample optimization using Microsoft Solver Foundation (MSF). The MSF is available through Nuget Package Manager.

---
+++ OptimizationProject/OptimizationProject/frmMSFuse.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace OptimizationProject
+{
+    partial class frmMSFuse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnMSFuse = new System.Windows.Forms.Button();
+            this.lblOut = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Maximize R = -2x + 5y, where x and y are integers,";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "100 <= x <= 200";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 104);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "80 <= y <= 170";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 138);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 17);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "y >= -x + 200";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 49);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "subject to";
+            // 
+            // btnMSFuse
+            // 
+            this.btnMSFuse.Location = new System.Drawing.Point(195, 124);
+            this.btnMSFuse.Name = "btnMSFuse";
+            this.btnMSFuse.Size = new System.Drawing.Size(136, 44);
+            this.btnMSFuse.TabIndex = 2;
+            this.btnMSFuse.Text = "Execute Sample";
+            this.btnMSFuse.UseVisualStyleBackColor = true;
+            this.btnMSFuse.Click += new System.EventHandler(this.btnMSFuse_Click);
+            // 
+            // lblOut
+            // 
+            this.lblOut.AutoSize = true;
+            this.lblOut.Location = new System.Drawing.Point(203, 59);
+            this.lblOut.Name = "lblOut";
+            this.lblOut.Size = new System.Drawing.Size(0, 17);
+            this.lblOut.TabIndex = 3;
+            // 
+            // frmMSFuse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(374, 189);
+            this.Controls.Add(this.lblOut);
+            this.Controls.Add(this.btnMSFuse);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmMSFuse";
+            this.Text = "Use of MSF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnMSFuse;
+        private System.Windows.Forms.Label lblOut;
+    }
+}
+
---
+++ OptimizationProject/OptimizationProject/frmMSFuse.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.SolverFoundation.Services;
+
+namespace OptimizationProject
+{
+    public partial class frmMSFuse : Form
+    {
+        public frmMSFuse()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMSFuse_Click(object sender, EventArgs e)
+        {
+            var solver = SolverContext.GetContext();
+            var model = solver.CreateModel();
+            //Add decision variables.
+            var decisionX = new Decision(Domain.IntegerNonnegative, "X");
+            var decisionY = new Decision(Domain.IntegerNonnegative, "Y");
+            model.AddDecision(decisionX);
+            model.AddDecision(decisionY);
+            //Add the objective or goal.
+            model.AddGoal("Goal", GoalKind.Maximize, -2 * decisionX + 5 * decisionY);
+            //Add constraints or subject to.
+            model.AddConstraint("Constraint0", 100 <= decisionX);
+            model.AddConstraint("Constraint1", decisionX <= 200);
+            model.AddConstraint("Constraint2", 80 <= decisionY);
+            model.AddConstraint("Constraint3", decisionY <= 170);
+            model.AddConstraint("Constraint4", decisionY >= -decisionX + 200);
+            //Solve and print outputs.
+            var solution = solver.Solve();
+            double x = decisionX.GetDouble();
+            double y = decisionY.GetDouble();
+            //System.Diagnostics.Trace.WriteLine("X = " + x + " y = " + y);
+            lblOut.Text= ("output: X = " + x + " y = " + y);
+        }
+    }
+}
